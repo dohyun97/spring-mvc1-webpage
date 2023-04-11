58d7da11f5424dd92cf40f70c54153c0c3b3c59b
dohyun97 <dohyun647@gmail.com>
2023-04-11 01:33:12 -0400

    redirect

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -35,16 +36,16 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                            @RequestParam Integer price,
-                            @RequestParam Integer quantity,
-                            Model model){
-        Item item =new Item(itemName,price,quantity);
-        itemRepository.save(item);
-        model.addAttribute("item",item);
-        return "basic/item";
-    }
+//    @PostMapping("/add")
+//    public String addItemV1(@RequestParam String itemName,
+//                            @RequestParam Integer price,
+//                            @RequestParam Integer quantity,
+//                            Model model){
+//        Item item =new Item(itemName,price,quantity);
+//        itemRepository.save(item);
+//        model.addAttribute("item",item);
+//        return "basic/item";
+//    }
 
       //model,@RequestParam,Item객체 생성 생략
 //    @PostMapping("/add")
@@ -55,17 +56,26 @@ public class BasicItemController {
 
      //modelAttribute 이름 생략. Model 이름은 클래스 이름(Item)을 소문자로 바꾼것. item
 //    @PostMapping("/add")
-//    public String addItemV2(@ModelAttribute Item item){
+//    public String addItemV3(@ModelAttribute Item item){
 //        itemRepository.save(item);
-//        return "basic/item";
+//        return "redirect:/basic/items/"+item.getId(); //인코딩으로 URL 작성이 안돼. {itemId}
 //    }
 
      //modelAttribute 생략
 //    @PostMapping("/add")
 //    public String addItemV4(Item item){
 //        itemRepository.save(item);
-//        return "basic/item";
+//        return "redirect:/basic/items/"+item.getId(); //인코딩으로 URL 작성이 안돼. {itemId}
 //    }
+
+    //Redirect 인코딩 으로 작성할 수 있게. 클라이언트에게 등록 후 "등록완료" 메세지 보내기
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, RedirectAttributes redirectAttributes){
+        itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",item.getId());
+        redirectAttributes.addAttribute("status",true); //status가 true면 등록완료 메세지 띄우기 - ?status=true
+        return "redirect:/basic/items/{itemId}";  //url: http://localhost:8080/basic/items/3?status=true
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model){
@@ -77,7 +87,7 @@ public class BasicItemController {
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId,@ModelAttribute Item item){
         itemRepository.update(itemId,item);
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/basic/items/{itemId}"; //pathvariable 값은 인코딩 가능.{itemId}
     }
 
     /**
